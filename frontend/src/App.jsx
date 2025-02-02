bedf946c8bd7e0362e7e91d24446f544c533a56e
Chirag101009 <chiraggupta1009.cg@gmail.com>
2025-02-02 18:29:06 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Navabar from './components/Navabar'
+import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
+      <Navabar/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/my-profile' element={<MyProfile />}/>
+        <Route path='/my-appointments' element={<MyAppointments />}/>
+        <Route path='/appointment/:docId' element={<Appointment />}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
